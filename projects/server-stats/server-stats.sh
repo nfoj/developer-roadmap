2a5b40d0033a85d6b228dd08e329f8750c2e6442
nfoj <nfoj_@hotmail.com>
2025-04-27 18:38:57 -0300

    [Roadmap] [Projects] [Beginner] Update - Output

--- projects/server-stats/server-stats.sh
+++ projects/server-stats/server-stats.sh
@@ -45,6 +45,7 @@ broadcast=$(ifconfig eth0 | grep 'inet ' | awk '{print $6}')
 
 # cpu - user
 cpu_user=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
+cpu_valor=$(echo "$cpu_user" | sed 's/%//')
 
 # disk - size
 disk_size=$(df -h | grep '^/dev/sd*' | awk '{ print $2 }' | cut -d'%' -f1)
@@ -72,23 +73,33 @@ mem_free_perc=$(free -h | grep 'Mem:' | awk '{printf "%.2f%%\n", ($4/$2)*100}')
 tempe=$(cat /sys/class/thermal/thermal_zone*/temp 2>/dev/null | awk '{printf "%.1f°C\n", $1/1000}')
 
 # process
-process=$(ps aux --sort=-%cpu | head -n 6 | awk 'NR == 1 { printf "%-10s %-6s %-5s %-5s %-20s %s\n", $1, $2, $3, $4, $9, $10 } NR > 1 { printf " %-9s %-6s %-5s %-5s %-20s %s\n", $1, $2, $3, $4, $9, $10 }')
+process=$(ps aux --sort=-%cpu | head -n 6 | awk 'NR == 1 { printf "%-6s %-6s %-6s %-6s %-7s %-7s %s\n", $1, $2, $3, $4, $9, $10, $11 } NR > 1 { printf " %-6s %-6s %-6s %-6s %-7s %-7s %s\n", $1, $2, $3, $4, $9, $
+10, $11 }')
 
-echo "+------------------------+"
-echo " Server Performance Stats "
-echo "+------------------------+"
-echo " User/Group: $user_group "
-echo "+------------------------+"
-echo " IP: $ip "
-echo " Netmask: $netmask "
-echo " Broadcast: $broadcast "
-echo "+------------------------+"
-echo " CPU: $cpu_user "
-echo "+------------------------+"
-echo " Disk Size: $disk_size "
-echo " Disk Used: $disk_used "
-echo " Disk Avail: $disk_avail "
-echo " Disk Use%: $disk_use_perc "
-echo "+------------------------+"
-echo " $process "
-
+#
+echo -e "${COLOR_BLUE}+--------------------------+${NO_COLOR}"
+echo -e "${COLOR_BLUE} Server Performance Stats ${NO_COLOR}"
+echo -e "${COLOR_BLUE}+--------------------------+${NO_COLOR}"
+echo -e "${COLOR_BLUE} User/Group:${NO_COLOR} $user_group "
+echo -e "${COLOR_BLUE}+--------------------------+${NO_COLOR}"
+echo -e "${COLOR_BLUE} IP:${NO_COLOR} $ip "
+echo -e "${COLOR_BLUE} Netmask:${NO_COLOR} $netmask "
+echo -e "${COLOR_BLUE} Broadcast:${NO_COLOR} $broadcast "
+echo -e "${COLOR_BLUE}+--------------------------+"
+printf "${COLOR_BLUE} CPU: ${NO_COLOR}%.2f%%\n" "$cpu_valor"
+echo -e "${COLOR_BLUE}+--------------------------+"
+echo -e "${COLOR_BLUE} Memory Total:${NO_COLOR} $mem_total "
+echo -e "${COLOR_BLUE} Memory User:${NO_COLOR} $mem_user "
+echo -e "${COLOR_BLUE} Memory Free:${NO_COLOR} $mem_free "
+echo -e "${COLOR_BLUE} Memory Free%:${NO_COLOR} $mem_free_perc "
+echo -e "${COLOR_BLUE}+--------------------------+${NO_COLOR}"
+echo -e "${COLOR_BLUE} Disk Size:${NO_COLOR} $disk_size "
+echo -e "${COLOR_BLUE} Disk Used:${NO_COLOR} $disk_used "
+echo -e "${COLOR_BLUE} Disk Avail:${NO_COLOR} $disk_avail "
+echo -e "${COLOR_BLUE} Disk Use%:${NO_COLOR} $disk_use_perc% "
+echo -e "${COLOR_BLUE}+--------------------------+"
+echo -e "${COLOR_BLUE} Temperature:${NO_COLOR} $tempe"
+echo -e "${COLOR_BLUE}+----------------------------------------------------+${NO_COLOR}"
+echo -e "${COLOR_BLUE} Top 5 processes (CPU/Memory):  ${NO_COLOR}"
+echo -e " $process "
+echo -e "${COLOR_BLUE}+----------------------------------------------------+${NO_COLOR}"
